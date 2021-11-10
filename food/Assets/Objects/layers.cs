d980745602d6a5754ed53201a0a581514e60adea
Lukyboy1 <75883919+Lukyboy1@users.noreply.github.com>
2021-11-10 22:49:07 +0100

    project
    unity project toegevoegd

---
+++ food/Assets/Objects/layers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer[] spriteRenderers = FindObjectsOfType<SpriteRenderer>();
+        foreach (SpriteRenderer renderer in spriteRenderers) { renderer.sortingOrder = (int) (renderer.transform.position.y*-25); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ food/Assets/Scenes/PlayerLayers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLayers : MonoBehaviour
+{
+    public SpriteRenderer playerRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        playerRenderer.sortingOrder = (int)(playerRenderer.transform.position.y * -25); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerRenderer.sortingOrder = (int)(playerRenderer.transform.position.y * -25);
+    }
+}
