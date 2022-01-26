727b755d3016ff0378ad53db36b735608ce56695
Lukyboy1 <75883919+Lukyboy1@users.noreply.github.com>
2022-01-26 16:57:29 +0100

    player animatie fix, lookdirection defined

--- food/Assets/Scripts/PlayerMovement.cs
+++ food/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     private Vector2 moveDirection;
+    private Vector2 lookDirection = new Vector2(0,-1);
 
     // Update is called once per frame
     void Update()
@@ -17,7 +18,18 @@ public class PlayerMovement : MonoBehaviour
 
         animator.SetFloat("Horizontal", moveDirection.x);
         animator.SetFloat("Vertical", moveDirection.y);
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rb.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast has hit the object " + hit.collider.gameObject);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -28,9 +40,14 @@ public class PlayerMovement : MonoBehaviour
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
-            float moveY = Input.GetAxisRaw("Vertical");
+        float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY);
+        if (!Mathf.Approximately(moveDirection.x, 0.0f) || !Mathf.Approximately(moveDirection.y, 0.0f))
+        {
+            lookDirection.Set(moveDirection.x, moveDirection.y);
+            lookDirection.Normalize();
+        }
     }
 
     void Move()
